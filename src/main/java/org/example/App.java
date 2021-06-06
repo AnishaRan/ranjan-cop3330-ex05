e928da0da5a40db6e711ee67a5b68e5d758bd01f
Anisha Desktop <anisharanjan55@gmail.com>
2021-06-05 23:58:11 -0400

    Solution w/ Comment

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,28 @@
 package org.example;
+import java.util.Scanner;
 
-/**
- * Hello world!
- *
+/*
+ *  UCF COP3330 Summer 2021 Assignment 5 Solution
+ *  Copyright 2021 Anisha Ranjan
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        Scanner scanNum1 = new Scanner(System.in);
+        Scanner scanNum2 = new Scanner(System.in);
+        System.out.print("What is the first number? ");
+        String str1 = scanNum1.nextLine();
+        System.out.print("What is the second number? ");
+        String str2 = scanNum2.nextLine();
+
+        int num1 = Integer.parseInt(str1);
+        int num2 = Integer.parseInt(str2);
+
+        int sum = num1 + num2;
+        int diff = num1 - num2;
+        int product = num1 * num2;
+        int quotient = num1 / num2;
+
+        System.out.printf("%d + %d = %d\n%d - %d = %d\n%d * %d = %d\n%d / %d = %d", num1, num2, sum, num1, num2, diff, num1, num2, product, num1, num2, quotient);
     }
 }
